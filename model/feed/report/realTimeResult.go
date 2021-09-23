a7184ad402b625a38cb7465b6d1e52e5da98a9bb
Syd Xu <prof.syd.xu@gmail.com>
2021-09-23 12:52:21 +0800

    fix(report): change RealtimeResult.ID to json.Number

--- model/feed/report/realTimeResult.go
+++ model/feed/report/realTimeResult.go
@@ -3,7 +3,7 @@ package report
 import "encoding/json"
 
 type RealTimeResult struct {
-	ID             string        `json:"ID,omitempty"`             // 请求对象的ID(ocpx报告、媒体ID报告，地域报告此字段无用，固定为0)
+	ID             json.Number   `json:"ID,omitempty"`             // 请求对象的ID(ocpx报告、媒体ID报告，地域报告此字段无用，固定为0)
 	Name           []string      `json:"name,omitempty"`           // 请求对象的name，数组形式，不同报告形式的不同定义规则见下文“name字段规则”
 	Date           string        `json:"date,omitempty"`           // 统计开始时间
 	KPIs           []json.Number `json:"KPIs,omitempty"`           // 按照请求顺序，返回KPI数据数组

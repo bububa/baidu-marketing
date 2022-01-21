534fb743763303c237d7ef1121bd7ebfaa25f653
Shinku <chenzhuyu@xd.com>
2022-01-21 16:22:37 +0800

    添加页码

--- model/feed/report/realTimeResult.go
+++ model/feed/report/realTimeResult.go
@@ -7,7 +7,7 @@ type RealTimeResult struct {
 	Name           []string      `json:"name,omitempty"`           // 请求对象的name，数组形式，不同报告形式的不同定义规则见下文“name字段规则”
 	Date           string        `json:"date,omitempty"`           // 统计开始时间
 	KPIs           []json.Number `json:"KPIs,omitempty"`           // 按照请求顺序，返回KPI数据数组
-	RelatedIdsList []int64       `json:"relatedIdsList,omitempty"` // 账户id，计划id，单元id
+	RelatedIdsList []json.Number `json:"relatedIdsList,omitempty"` // 账户id，计划id，单元id
 	TotalRowNumber json.Number   `json:"totalRowNumber,omitempty"` // 记录总条数
 	PageIndex      json.Number   `json:"pageindex,omitempty"`      // 当前页码
 }

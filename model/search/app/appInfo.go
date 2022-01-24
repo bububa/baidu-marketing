4e36830d38f6ffa101a21535c47cad574699d3cc
Shinku <chenzhuyu@xd.com>
2022-01-24 16:54:49 +0800

    添加页码

--- model/search/app/appInfo.go
+++ model/search/app/appInfo.go
@@ -1,17 +1,20 @@
 package app
 
 type InfoApp struct {
-	Name        string `json:"name"`
-	Version     string `json:"version"`
-	Platform    int    `json:"platform"`
-	VersionId   int    `json:"versionId"`
-	AppId       int    `json:"appId"`
-	PackageName string `json:"packageName"`
-	Status      int    `json:"status"`
-	AppSource   int    `json:"appSource"`
-	ChannelId   int    `json:"channelId"`
-	AppStoreId  int    `json:"appStoreId"`
-	Icon        string `json:"icon"`
-	DownloadUrl string `json:"downloadUrl"`
-	ChannelName string `json:"channelName"`
+	AppInfoList []struct {
+		Name        string `json:"name"`
+		Version     string `json:"version"`
+		Platform    int    `json:"platform"`
+		VersionId   int    `json:"versionId"`
+		AppId       int    `json:"appId"`
+		PackageName string `json:"packageName"`
+		Status      int    `json:"status"`
+		AppSource   int    `json:"appSource"`
+		ChannelId   int    `json:"channelId"`
+		AppStoreId  int    `json:"appStoreId"`
+		Icon        string `json:"icon"`
+		DownloadUrl string `json:"downloadUrl"`
+		ChannelName string `json:"channelName"`
+	} `json:"appInfoList"`
+	TotalCount int `json:"totalCount"`
 }

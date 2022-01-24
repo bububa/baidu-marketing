6577fa7e8e45a34463a7f7555b92c139019c626d
Shinku <chenzhuyu@xd.com>
2022-01-24 16:35:38 +0800

    添加页码

--- api/search/app/getAdgroupAppBind.go
+++ api/search/app/getAdgroupAppBind.go
@@ -1,1 +1,20 @@
 package app
+
+import (
+	"github.com/bububa/baidu-marketing/core"
+	"github.com/bububa/baidu-marketing/model"
+	"github.com/bububa/baidu-marketing/model/search/app"
+)
+
+func GetAdgroupAppBind(clt *core.SDKClient, auth model.RequestHeader, reqBody *app.GetAdgroupAppBindRequest) ([]app.InfoApp, error) {
+	req := &model.Request{
+		Header: auth,
+		Body:   reqBody,
+	}
+	var resp app.GetAdgroupAppBindResponse
+	err := clt.Do(req, &resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Data, nil
+}
